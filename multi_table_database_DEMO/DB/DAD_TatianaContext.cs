ae015d042ddfe8cab62c616bb32b642717264f12
Tatiana Pasechnik <81549043+TatianaPas@users.noreply.github.com>
2022-05-06 14:03:49 +1200

    Update DAD_TatianaContext.cs

--- multi_table_database_DEMO/DB/DAD_TatianaContext.cs
+++ multi_table_database_DEMO/DB/DAD_TatianaContext.cs
@@ -24,7 +24,7 @@ namespace multi_table_database_DEMO.DB
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=citizen.manukautech.info,6306;Initial Catalog=DAD_Tatiana;Persist Security Info=True;User ID=DAD_Tatiana;Password=Transcom01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=citizen.manukautech.info,6306;Initial Catalog=DAD_Tatiana;Persist ;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
             }
         }
 
